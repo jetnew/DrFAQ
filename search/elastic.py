add701eed408bd80b4daf346663a0e902ca3d774
jetnew <notesjet@gmail.com>
2019-12-09 11:10:02 +0800

    Included bonsai elasticsearch

--- search/elastic.py
+++ search/elastic.py
@@ -1,3 +1,4 @@
+import os, base64, re, logging
 from elasticsearch import Elasticsearch
 import time
 
@@ -11,7 +12,37 @@ Ref: https://elasticsearch-py.readthedocs.io/en/master/
 
 class Search:
     def __init__(self):
-        self.elastic = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+        # Local
+        # self.elastic = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+        # Bonsai Deployment
+        # Log transport details (optional):
+        logging.basicConfig(level=logging.INFO)
+
+        # Parse the auth and host from env:
+        bonsai = os.environ['BONSAI_URL']
+        auth = re.search('https\:\/\/(.*)\@', bonsai).group(1).split(':')
+        host = bonsai.replace('https://%s:%s@' % (auth[0], auth[1]), '')
+
+        # optional port
+        match = re.search('(:\d+)', host)
+        if match:
+            p = match.group(0)
+            host = host.replace(p, '')
+            port = int(p.split(':')[1])
+        else:
+            port = 443
+
+        # Connect to cluster over SSL using auth for best security:
+        es_header = [{
+            'host': host,
+            'port': port,
+            'use_ssl': True,
+            'http_auth': (auth[0], auth[1])
+        }]
+
+        # Instantiate the new Elasticsearch connection:
+        self.elastic = Elasticsearch(es_header)
 
     def load(self, id, text, verbose=False):
         """Loads documents in the format {'text': text}."""
